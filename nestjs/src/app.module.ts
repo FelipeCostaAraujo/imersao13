e5fdad69e76a4e005e8d5ba454a449be1a7ac754
Felipe C. Araujo <felipe.s-04@hotmail.com>
2023-06-27 14:21:52 -0300

    final da ultima aula

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AssetsModule } from './assets/assets.module';
-import { WalletsModule } from './wallets/wallets.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [PrismaModule, AssetsModule, WalletsModule, OrdersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
